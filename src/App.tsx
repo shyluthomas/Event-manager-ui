a05b1c63674f824a16dfd6e1f7de1c5bd0b5c8e2
BlesyMathew <blessymathew2016@gmail.com>
2024-02-10 19:30:54 +0530

    testing do not merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import { useRoutes } from "react-router-dom";
+
 import Routes from "./utils/routes";
+import { useRoutes } from "react-router-dom";
 
 function App() {
   const routes = useRoutes(Routes);
+  console.log('routes',routes)
   return <>{routes}</>;
 }
 
